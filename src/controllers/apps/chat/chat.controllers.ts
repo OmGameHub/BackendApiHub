ec9cdac8f251b1f6f5f0839bc73b98883103c40e
Om Prakash Pandey <pandeyomu16.gol@gmail.com>
2024-09-28 22:17:32 +0530

    add: group chat

--- src/controllers/apps/chat/chat.controllers.ts
+++ src/controllers/apps/chat/chat.controllers.ts
@@ -111,6 +111,430 @@ export const createOrGetAOneOnOneChat = asyncHandler(async (req, res) => {
     .json(new ApiResponse(201, "Chat retrieved successfully", selectedChat));
 });
 
+export const createGroupChat = asyncHandler(async (req, res) => {
+  const loggedInUser = req.user;
+  const { name, memberIds = [] } = req.body;
+
+  if (!name) {
+    throw new ApiError(400, "Group name is required");
+  }
+
+  const members = await dbClient.users.findMany({
+    where: {
+      uuid: {
+        in: memberIds,
+      },
+    },
+  });
+
+  const groupMemberIds = members.map((member) => member.id);
+  if (!groupMemberIds.includes(loggedInUser.id)) {
+    groupMemberIds.push(loggedInUser.id);
+  }
+
+  if (groupMemberIds.length < 2) {
+    throw new ApiError(400, "Members are required to create a group chat");
+  }
+
+  const chatGroup = await dbClient.chatChannels.create({
+    data: {
+      isGroupChat: true,
+      createdByUserId: loggedInUser.id,
+      name,
+      members: {
+        create: groupMemberIds.map((memberId) => ({
+          user: {
+            connect: {
+              id: memberId,
+            },
+          },
+          role:
+            memberId === loggedInUser.id
+              ? UserRolesEnum.ADMIN
+              : UserRolesEnum.USER,
+        })) as Prisma.ChatChannelMembersCreateWithoutChatChannelInput[],
+      },
+    },
+  });
+
+  const whereQuery = `chat.id = '${chatGroup.id}'`;
+  const selectedChat = await selectAllDBQChatChannels(
+    whereQuery,
+    loggedInUser.id
+  );
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, "Chat retrieved successfully", selectedChat));
+});
+
+export const getAllChatChannels = asyncHandler(async (req, res) => {
+  const loggedInUser = req.user;
+  let { page = 1, limit = 10, isGroupChat } = apiQueryBuilder(req.query);
+  const skip = (page - 1) * limit;
+
+  if (isGroupChat) {
+    isGroupChat = isGroupChat === "true";
+  }
+
+  const query: Prisma.ChatChannelsWhereInput = {
+    members: {
+      some: {
+        userId: loggedInUser.id,
+      },
+    },
+  };
+
+  if (isGroupChat === "true") {
+    query.isGroupChat = true;
+  }
+
+  const metaData = await getPaginationMetaData(dbClient.chatChannels, {
+    query,
+    page,
+    limit,
+  });
+
+  const whereQuery =
+    typeof isGroupChat === "boolean"
+      ? `chat."isGroupChat" = ${isGroupChat}`
+      : `1 = 1`;
+  const chatChannels = await selectAllDBQChatChannels(
+    whereQuery,
+    loggedInUser.id,
+    limit,
+    skip
+  );
+
+  return res.status(200).json(
+    new ApiResponse(200, "Chat channels fetched successfully", {
+      list: chatChannels,
+      meta: metaData,
+    })
+  );
+});
+
+export const getOneGroupChat = asyncHandler(async (req, res) => {
+  const loggedInUser = req.user;
+  const { chatId } = req.params;
+
+  const chatGroup = await dbClient.chatChannels.findFirst({
+    where: {
+      uuid: chatId,
+      isGroupChat: true,
+      members: {
+        some: {
+          userId: loggedInUser.id,
+        },
+      },
+    },
+  });
+  if (!chatGroup) {
+    throw new ApiError(404, "Chat group does not exist");
+  }
+
+  const selectedChat = await selectAllDBQChatChannels(
+    `chat.id = '${chatGroup.id}'`,
+    loggedInUser.id
+  );
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, "Chat group retrieved successfully", selectedChat)
+    );
+});
+
+export const updateGroupChatDetails = asyncHandler(async (req, res) => {
+  const loggedInUser = req.user;
+  const { chatId } = req.params;
+  const { name } = req.body;
+
+  const chatGroup = await dbClient.chatChannels.findFirst({
+    where: {
+      uuid: chatId,
+      isGroupChat: true,
+      members: {
+        some: {
+          userId: loggedInUser.id,
+        },
+      },
+    },
+    include: {
+      members: true,
+    },
+  });
+
+  if (!chatGroup) {
+    throw new ApiError(404, "Chat group does not exist");
+  }
+
+  const member = chatGroup.members.find((m) => m.userId == loggedInUser.id);
+  if (!member || member.role !== UserRolesEnum.ADMIN) {
+    throw new ApiError(403, "You are not authorized to update this chat group");
+  }
+
+  const updatedChatGroup = await dbClient.chatChannels.update({
+    where: { id: chatGroup.id },
+    data: {
+      name,
+    },
+  });
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, "Chat group updated successfully", updatedChatGroup)
+    );
+});
+
+export const changeMemberRoleInGroupChat = asyncHandler(async (req, res) => {
+  const { chatId, memberId } = req.params;
+  const loggedInUser = req.user;
+  const { role = "" } = req.body;
+
+  if (!role || !(role in UserRolesEnum)) {
+    throw new ApiError(400, "Invalid role");
+  }
+
+  const member = await dbClient.users.findUnique({
+    where: { uuid: memberId },
+  });
+  if (!member) {
+    throw new ApiError(404, "User does not exist");
+  }
+
+  const chatGroup = await dbClient.chatChannels.findFirst({
+    where: {
+      uuid: chatId,
+      isGroupChat: true,
+      members: {
+        some: {
+          userId: loggedInUser.id,
+        },
+      },
+    },
+    include: {
+      members: true,
+    },
+  });
+
+  if (!chatGroup) {
+    throw new ApiError(404, "Chat group does not exist");
+  }
+
+  const mMember = chatGroup.members.find((m) => m.userId == loggedInUser.id);
+  if (!mMember || mMember.role !== UserRolesEnum.ADMIN) {
+    throw new ApiError(
+      403,
+      "You are not authorized to change member role in this chat group"
+    );
+  }
+
+  const isMemberInGroup = chatGroup.members.some((m) => m.userId == member.id);
+  if (!isMemberInGroup) {
+    throw new ApiError(400, "Member does not exist in this chat group");
+  }
+
+  await dbClient.chatChannelMembers.update({
+    where: {
+      userId_chatChannelId: {
+        userId: member.id,
+        chatChannelId: chatGroup.id,
+      },
+    },
+    data: {
+      role,
+    },
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Member role changed successfully"));
+});
+
+export const addMembersInGroupChat = asyncHandler(async (req, res) => {
+  const { chatId, memberId } = req.params;
+  const loggedInUser = req.user;
+
+  const chatGroup = await dbClient.chatChannels.findFirst({
+    where: {
+      uuid: chatId,
+      isGroupChat: true,
+      members: {
+        some: {
+          userId: loggedInUser.id,
+        },
+      },
+    },
+    include: {
+      members: true,
+    },
+  });
+
+  if (!chatGroup) {
+    throw new ApiError(404, "Chat group does not exist");
+  }
+
+  const mMember = chatGroup.members.find((m) => m.userId == loggedInUser.id);
+  if (!mMember || mMember.role !== UserRolesEnum.ADMIN) {
+    throw new ApiError(
+      403,
+      "You are not authorized to add members to this chat group"
+    );
+  }
+
+  const participant = await dbClient.users.findUnique({
+    where: { uuid: memberId },
+  });
+
+  if (!participant) {
+    throw new ApiError(404, "Member does not exist");
+  }
+
+  const member = chatGroup.members.find((m) => m.userId == participant.id);
+  if (member) {
+    throw new ApiError(400, "Member already exists in this chat group");
+  }
+
+  await dbClient.chatChannelMembers.create({
+    data: {
+      chatChannelId: chatGroup.id,
+      userId: participant.id,
+    },
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Member added successfully"));
+});
+
+export const removeMemberFromChatGroup = asyncHandler(async (req, res) => {
+  const { chatId, memberId } = req.params;
+  const loggedInUser = req.user;
+
+  const chatGroup = await dbClient.chatChannels.findFirst({
+    where: {
+      uuid: chatId,
+      isGroupChat: true,
+      members: {
+        some: {
+          userId: loggedInUser.id,
+        },
+      },
+    },
+    include: {
+      members: true,
+    },
+  });
+
+  if (!chatGroup) {
+    throw new ApiError(404, "Chat group does not exist");
+  }
+
+  const mMember = chatGroup.members.find((m) => m.userId == loggedInUser.id);
+  if (!mMember || mMember.role !== UserRolesEnum.ADMIN) {
+    throw new ApiError(
+      403,
+      "You are not authorized to remove members from this chat group"
+    );
+  }
+
+  const participant = await dbClient.users.findUnique({
+    where: { uuid: memberId },
+  });
+
+  if (!participant) {
+    throw new ApiError(404, "Member does not exist");
+  }
+
+  const member = chatGroup.members.find((m) => m.userId == participant.id);
+  if (!member) {
+    throw new ApiError(400, "Member does not exist in this chat group");
+  }
+
+  await dbClient.chatChannelMembers.delete({
+    where: {
+      userId_chatChannelId: {
+        userId: participant.id,
+        chatChannelId: chatGroup.id,
+      },
+    },
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Member removed successfully"));
+});
+
+export const leaveGroupChat = asyncHandler(async (req, res) => {
+  const loggedInUser = req.user;
+  const { chatId } = req.params;
+
+  const chatGroup = await dbClient.chatChannels.findFirst({
+    where: {
+      uuid: chatId,
+      isGroupChat: true,
+      members: {
+        some: {
+          userId: loggedInUser.id,
+        },
+      },
+    },
+  });
+  if (!chatGroup) {
+    throw new ApiError(404, "Chat group does not exist");
+  }
+
+  await dbClient.chatChannelMembers.delete({
+    where: {
+      userId_chatChannelId: {
+        userId: loggedInUser.id,
+        chatChannelId: chatGroup.id,
+      },
+    },
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "You have left the chat group"));
+});
+
+export const deleteGroupChat = asyncHandler(async (req, res) => {
+  const loggedInUser = req.user;
+  const { chatId } = req.params;
+
+  const chatGroup = await dbClient.chatChannels.findFirst({
+    where: {
+      uuid: chatId,
+      isGroupChat: true,
+      members: {
+        some: {
+          userId: loggedInUser.id,
+        },
+      },
+    },
+    include: {
+      members: true,
+    },
+  });
+  if (!chatGroup) {
+    throw new ApiError(404, "Chat group does not exist");
+  }
+
+  const member = chatGroup.members.find((m) => m.userId == loggedInUser.id);
+  if (!member || member.role !== UserRolesEnum.ADMIN) {
+    throw new ApiError(403, "You are not authorized to delete this chat group");
+  }
+
+  await dbClient.chatChannels.delete({
+    where: { id: chatGroup.id },
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Chat group deleted successfully"));
+});
+
 const selectAllDBQChatChannels = async (
   whereQuery: string,
   userId = 0,
--- src/routes/app/chat/chat.routes.ts
+++ src/routes/app/chat/chat.routes.ts
@@ -3,14 +3,41 @@ import { verifyJWT } from "@/middlewares/auth.middleware";
 import {
   getAllAvailableUsers,
   createOrGetAOneOnOneChat,
+  createGroupChat,
+  getAllChatChannels,
+  getOneGroupChat,
+  updateGroupChatDetails,
+  changeMemberRoleInGroupChat,
+  addMembersInGroupChat,
+  removeMemberFromChatGroup,
+  deleteGroupChat,
+  leaveGroupChat,
 } from "@/controllers/apps/chat/chat.controllers";
 
 const router = Router();
 
 router.use(verifyJWT);
 
+router.get("/", getAllChatChannels);
+
 router.get("/users", getAllAvailableUsers);
 
 router.get("/c/:receiverId", createOrGetAOneOnOneChat);
+
+router.post("/group", createGroupChat);
+
+router
+  .route("/group/:chatId")
+  .get(getOneGroupChat)
+  .put(updateGroupChatDetails)
+  .delete(deleteGroupChat);
+
+router
+  .route("/group/:chatId/:memberId")
+  .post(addMembersInGroupChat)
+  .patch(changeMemberRoleInGroupChat)
+  .delete(removeMemberFromChatGroup);
+
+router.route("/leave/group/:chatId").delete(leaveGroupChat);
 
 export default router;
